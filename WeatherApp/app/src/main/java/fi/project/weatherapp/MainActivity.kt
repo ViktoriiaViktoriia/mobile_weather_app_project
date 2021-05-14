bf25c7ffc905e84ed7eeccd5e62af0a85a13023b
ViktoriiaViktoriia <77579932+ViktoriiaViktoriia@users.noreply.github.com>
2021-05-14 22:55:15 +0300

    Update MainActivity.kt

--- WeatherApp/app/src/main/java/fi/project/weatherapp/MainActivity.kt
+++ WeatherApp/app/src/main/java/fi/project/weatherapp/MainActivity.kt
@@ -1,6 +1,7 @@
 package fi.project.weatherapp
 
 import android.content.Intent
+import android.os.Build
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
 import android.widget.Button
@@ -11,10 +12,12 @@ import java.net.URL;
 import org.json.JSONObject;
 import android.util.Log;
 import android.view.View
-import java.net.HttpURLConnection
-import java.net.URLEncoder
-import javax.net.ssl.HttpsURLConnection
-
+import androidx.annotation.RequiresApi
+import java.time.LocalDateTime
+import java.time.LocalTime
+import java.time.format.DateTimeFormatter
+import java.time.format.FormatStyle
+import kotlin.concurrent.thread
 
 class MainActivity : AppCompatActivity() {
 
@@ -22,6 +25,7 @@ class MainActivity : AppCompatActivity() {
     private var btn: Button? = null
     private var output: TextView? = null
 
+    @RequiresApi(Build.VERSION_CODES.O)
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
@@ -36,22 +40,31 @@ class MainActivity : AppCompatActivity() {
             if(input?.text?.toString()?.trim()?.equals("")!!)
                 Toast.makeText(this, "Enter a city name", Toast.LENGTH_LONG).show()
             else {
-                var city: String = input?.text.toString()
-                //var key: String = "6b939df2cb378451fd7012e6b3250865"
-                var url: String = "https://api.openweathermap.org/data/2.5/weather?q=$city&units=metric&appid=6b939df2cb378451fd7012e6b3250865"
 
-                /*
-                val apiResponse = URL(url).readText()         //http connection error
-                val weather = JSONObject(apiResponse).getJSONArray("weather")
-                val desc = weather.getJSONObject(0).getString("description")
-                val main = JSONObject(apiResponse).getJSONObject("main")
-                val temp = main.getString("temp")
+                thread() {
 
-                output?.text = "Temperature: $temp/n$desc"
+                    var city: String = input?.text.toString()
+                    var key: String = "6b939df2cb378451fd7012e6b3250865"
+                    var url: String = "https://api.openweathermap.org/data/2.5/weather?q=$city&units=metric&appid=$key"
+                    
+                    val apiResponse = URL(url).readText()                                   //.readText(Charsets.UTF_8)
+                    val weather = JSONObject(apiResponse).getJSONArray("weather")
+                    val desc = weather.getJSONObject(0).getString("description")
+                    val main = JSONObject(apiResponse).getJSONObject("main")
+                    val temp = main.getString("temp")
+                    val pressure = main.getString("pressure")
+                    val humidity = main.getString("humidity")
 
-                */
+                    val date = LocalDateTime.now()
+                    val currentdate = date.format(DateTimeFormatter.ofLocalizedDate(FormatStyle.FULL))
+                    val time = LocalTime.now().format(DateTimeFormatter.ofLocalizedTime(FormatStyle.SHORT))
 
-                output?.text = "City: Tampere      Temperature: 1.97    light rain  Pressure: 1013 Humidity: 93"
+                    runOnUiThread() {
+                        output?.text = "$currentdate, $time\n \nTemperature: $temp°  $desc  \nPressure: $pressure hPa  \nHumidity: $humidity % "
+
+                    }
+                    Thread.sleep(1000)
+                }
 
                 startService(Intent(this, MyBackgroundService::class.java))
                 Log.d("MyBackgroundService", "Service started: $output")
@@ -61,3 +74,6 @@ class MainActivity : AppCompatActivity() {
 
     }
 }
+
+
+
